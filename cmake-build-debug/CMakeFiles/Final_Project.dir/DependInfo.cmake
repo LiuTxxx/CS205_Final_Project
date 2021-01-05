629515dde971fe1d7a785207c7aa03ada7ba7cf0
Liu <11910903@mail.sustech.edu.cn>
2021-01-05 14:39:25 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/Final_Project.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/SeeE/CLionProjects/Final_Project/main.cpp" "C:/Users/SeeE/CLionProjects/Final_Project/cmake-build-debug/CMakeFiles/Final_Project.dir/main.cpp.obj"
+  "C:/Users/SeeE/CLionProjects/Final_Project/src/face_binary_cls.cpp" "C:/Users/SeeE/CLionProjects/Final_Project/cmake-build-debug/CMakeFiles/Final_Project.dir/src/face_binary_cls.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../"
+  "E:/OpenBLAS/include"
+  "E:/opencv/new_build"
+  "E:/opencv/sources/include"
+  "E:/opencv/sources/modules/core/include"
+  "E:/opencv/sources/modules/flann/include"
+  "E:/opencv/sources/modules/imgproc/include"
+  "E:/opencv/sources/modules/ml/include"
+  "E:/opencv/sources/modules/photo/include"
+  "E:/opencv/sources/modules/dnn/include"
+  "E:/opencv/sources/modules/features2d/include"
+  "E:/opencv/sources/modules/imgcodecs/include"
+  "E:/opencv/sources/modules/videoio/include"
+  "E:/opencv/sources/modules/calib3d/include"
+  "E:/opencv/sources/modules/highgui/include"
+  "E:/opencv/sources/modules/objdetect/include"
+  "E:/opencv/sources/modules/stitching/include"
+  "E:/opencv/sources/modules/ts/include"
+  "E:/opencv/sources/modules/video/include"
+  "E:/opencv/sources/modules/gapi/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Final_Project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Final_Project.dir/main.cpp.obj"
+  "CMakeFiles/Final_Project.dir/src/face_binary_cls.cpp.obj"
+  "Final_Project.exe"
+  "Final_Project.exe.manifest"
+  "Final_Project.pdb"
+  "libFinal_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Final_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
